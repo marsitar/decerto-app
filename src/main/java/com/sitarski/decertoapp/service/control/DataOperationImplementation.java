57318c7041e3187f2617395fdf80425c2a60eb1d
marek sitarski <marek_sitarski@vp.pl>
2021-02-21 23:56:06 +0100

    ref #0003 - add DataOperation interface with calculate method, and implement it in DataOperationImplementation class

--- src/main/java/com/sitarski/decertoapp/service/control/DataOperationImplementation.java
+++ src/main/java/com/sitarski/decertoapp/service/control/DataOperationImplementation.java
@@ -1,4 +1,23 @@
 package com.sitarski.decertoapp.service.control;
 
-public class DataOperationImplementation {
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@RequestScope
+@Service
+public class DataOperationImplementation implements DataOperation<Integer> {
+
+    @Override
+    public Integer calculate(Collection<Integer> numbers) {
+
+        return Optional.ofNullable(numbers)
+                .orElse(Collections.emptyList())
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
 }
