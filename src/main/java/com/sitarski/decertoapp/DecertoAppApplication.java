7511c9788273f6eabc0cd1796cec2f33706ccfb2
marek sitarski <marek_sitarski@vp.pl>
2021-02-21 21:05:06 +0100

    ref #0001 - configure basic spring boot framework

---
+++ src/main/java/com/sitarski/decertoapp/DecertoAppApplication.java
@@ -0,0 +1,13 @@
+package com.sitarski.decertoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecertoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecertoAppApplication.class, args);
+	}
+
+}
